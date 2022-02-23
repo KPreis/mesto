0cd6aab325dd226aefd3172cd4906e0aac1b100c
Admin <admin@Admins-MacBook-Pro.local>
2022-02-23 21:39:40 +0300

    fix: renamed modal to pop-up

--- scripts/script.js
+++ scripts/script.js
@@ -3,30 +3,29 @@ let profileName = profileInfo.querySelector('.profile__name');
 let profileDescription = profileInfo.querySelector('.profile__description');
 const editProfileButton = profileInfo.querySelector('.profile__edit-button');
 
-const editModalWindow = document.querySelector('.modal');
-const closeButtonModalWindow = editModalWindow.querySelector('.modal__close-button');
-const saveButtonModalWindow = editModalWindow.querySelector('.modal__save-button');
-let editNameField = editModalWindow.querySelector('.modal__name-field');
-let editDescriptionField = editModalWindow.querySelector('.modal__description-field');
+const editPopUp = document.querySelector('.pop-up');
+const closeButtonPopUp = editPopUp.querySelector('.pop-up__close-button');
+const saveButtonPopUp = editPopUp.querySelector('.pop-up__save-button');
+let editNameField = editPopUp.querySelector('.pop-up__name-field');
+let editDescriptionField = editPopUp.querySelector('.pop-up__description-field');
 
-function openEditModal() {
+function openEditPopUp() {
   editNameField.value = profileName.outerText;
   editDescriptionField.value = profileDescription.textContent;
-  editModalWindow.setAttribute('style', 'display: flex');
+  editPopUp.setAttribute('style', 'display: flex');
 }
 
-function closeEditModal() {
-  editModalWindow.setAttribute('style', 'display: none');
+function closeEditPopUp() {
+  editPopUp.setAttribute('style', 'display: none');
 }
 
 function saveEditData() {
   profileName.childNodes[0].data = editNameField.value;
   profileDescription.textContent = editDescriptionField.value;
-  closeEditModal();
-  console.dir(editProfileButton);
+  closeEditPopUp();
 }
 
 
-editProfileButton.addEventListener('click', openEditModal);
-closeButtonModalWindow.addEventListener('click', closeEditModal);
-saveButtonModalWindow.addEventListener('click', saveEditData);
+editProfileButton.addEventListener('click', openEditPopUp);
+closeButtonPopUp.addEventListener('click', closeEditPopUp);
+saveButtonPopUp.addEventListener('click', saveEditData);
