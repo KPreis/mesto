2044cb2c46e6299dddfa670a152ffbf78b10c4ac
Konstantin Preis <kpreis@parallels.com>
2022-03-19 18:05:28 +0300

    feat: added delete icon and creating cards from array

--- scripts/script.js
+++ scripts/script.js
@@ -9,6 +9,49 @@ const closeButtonPopUp = editPopUp.querySelector('.edit-form__close-button');
 let editNameField = editPopUp.querySelector('#name-field');
 let editDescriptionField = editPopUp.querySelector('#description-field');
 
+const galary = document.querySelector('.galary');
+const cardList = galary.querySelector('.galary__cards-list');
+
+const initialCards = [
+  {
+    name: 'Таганай',
+    link: 'https://mesto.kpreis.ru/images/taganay.jpeg'
+  },
+  {
+    name: 'Двуглавая сопка',
+    link: 'https://mesto.kpreis.ru/images/dvuglavaya-sopka.jpeg'
+  },
+  {
+    name: 'Откликной Гребень',
+    link: 'https://mesto.kpreis.ru/images/otkliknoy-greben.jpeg'
+  },
+  {
+    name: 'Круглица',
+    link: 'https://mesto.kpreis.ru/images/kruglitsa.webp'
+  },
+  {
+    name: 'Большая Каменная Река',
+    link: 'https://mesto.kpreis.ru/images/kamennaya-reka.jpeg'
+  },
+  {
+    name: 'Метеостанция',
+    link: 'https://mesto.kpreis.ru/images/meteostanciya-taganay.jpeg'
+  }
+];
+
+function addCard(name='', link='') {
+  const cardTemplate = document.querySelector('#card').content;
+  const newCard = cardTemplate.querySelector('.card').cloneNode(true);
+  newCard.querySelector('.card__img').src = link;
+  newCard.querySelector('.card__img').alt = name;
+  newCard.querySelector('.card__name').textContent = name;
+  return cardList.append(newCard);
+}
+
+initialCards.forEach(element => {
+  addCard(element.name, element.link) 
+});
+
 function openEditPopUp() {
   editNameField.value = profileName.textContent;
   editDescriptionField.value = profileDescription.textContent;
