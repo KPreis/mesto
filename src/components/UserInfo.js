b148b117e72f0ff73e77feb63400fae9553802d2
Konstantin Preis <kpreis@parallels.com>
2022-05-17 09:34:10 +0300

    feat: created class UserInfo

---
+++ src/components/UserInfo.js
@@ -0,0 +1,20 @@
+export default class UserInfo {
+  constructor(selectorName, selectorDescriptions) {
+    this._fieldName = document.querySelector(selectorName);
+    this._fieldDescriptions = document.querySelector(selectorDescriptions);
+  }
+
+  getUserInfo = () => {
+    const userInfoValues = {
+      name: this._fieldName.textContent,
+      description: this._fieldDescriptions.textContent,
+    };
+
+    return userInfoValues;
+  };
+
+  setUserInfo = (name, description) => {
+    this._fieldName.textContent = name;
+    this._fieldDescriptions.textContent = description;
+  };
+}
